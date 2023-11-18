b59216c7078cc1000ded345eb044bb9c0b1faa05
IlyaMuravjov <muravjovilya@gmail.com>
2023-11-19 01:51:19 +0300

    Solve task 1

---
+++ Task1/Task1/FieldAccessor.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FieldAccessor
+{
+    public interface IFieldAccessorFactory
+    {
+        public Func<T, U?> GetFieldAccessor<T, U>(string fieldPath);
+    }
+
+    public class ExpressionBasedFieldAccessorFactory : IFieldAccessorFactory
+    {
+        public Func<T, U?> GetFieldAccessor<T, U>(string fieldPath)
+        {
+            ParameterExpression param = Expression.Parameter(typeof(T), "value");
+            Expression body = fieldPath
+                .Split(".")
+                .Aggregate((Expression)param, Expression.Field);
+            return Expression.Lambda<Func<T, U>>(body, [param]).Compile();
+        }
+    }
+
+    public class ReflectionBasedFieldAccessorFactory : IFieldAccessorFactory
+    {
+
+        public Func<T, U?> GetFieldAccessor<T, U>(string fieldPath) =>
+            (T value) => (U?)fieldPath.Split(".").Aggregate((object?)value, GetFieldValue);
+
+        private static object? GetFieldValue(object? value, string fieldName) => (
+                (value ?? throw new NullReferenceException($"null.{fieldName}"))
+                    .GetType()
+                    .GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                    ?? throw new ArgumentException($"Field '{fieldName}' not found in type '{value.GetType()}'")
+            ).GetValue(value);
+    }
+}
